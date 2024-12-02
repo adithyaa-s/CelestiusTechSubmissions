a13c9123147d9853d0c57b990d42f252ef975916
ADITHYAA SARAVANAM <adithyaaasaravanam@gmail.com>
2024-12-02 08:35:52 +0300

    Tech Submission

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,29 +1,38 @@
-import './globals.css'
+import type { Metadata } from "next";
+import localFont from "next/font/local";
 import { Inter } from 'next/font/google'
-import { GeistSans, GeistMono } from '@geist-ui/react'
+import "./globals.css";
 
 const inter = Inter({ subsets: ['latin'] })
-const geistSans = GeistSans()
-const geistMono = GeistMono()
 
-export const metadata = {
-  title: 'Student Project Submission',
-  description: 'Submit your project details',
-}
+const geistSans = localFont({
+  src: "./fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+  weight: "100 900",
+});
+const geistMono = localFont({
+  src: "./fonts/GeistMonoVF.woff",
+  variable: "--font-geist-mono",
+  weight: "100 900",
+});
+
+export const metadata: Metadata = {
+  title: "Tech Submissions",
+  description: "Submit your application for Tech Selection",
+};
 
 export default function RootLayout({
   children,
-}: {
-  children: React.ReactNode
-}) {
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable} ${inter.className} antialiased`}>
-        <div className="min-h-screen bg-gradient-to-br from-gray-900 via-purple-900 to-violet-600">
-          {children}
-        </div>
+      <body
+        // className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={inter.className}>{children}
+        {children}
       </body>
     </html>
   );
 }
-
